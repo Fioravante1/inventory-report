75ca0aba6a46f15f2aff4a621cd0d7faa906f5cc
EderPaiva <edergomesdepaiva12@gmail.com>
2021-12-12 19:18:35 -0300

    requisito 1 feito depois de luta

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -2,9 +2,26 @@ from datetime import date
 
 
 class SimpleReport:
-    def next_date(dict_list):
-        today = date.today().strftime('%Y-%m-%d')
-        return today
+    def count_enterprise(enterprise_name, dict_list):
+        value = 0
+        for enterprise in dict_list:
+            if enterprise["nome_da_empresa"] == enterprise_name:
+                value += 1
+
+        return value
+
+    def closest_expiration_date(dict_list):
+        date_list = []
+        today = date.today()
+        for current_date in dict_list:
+            date_list.append(
+                date.fromisoformat(
+                    current_date["data_de_validade"]
+                                        )
+                        )
+        # https://stackoverflow.com/questions/32237862/find-the-closest-date-to-a-given-date
+        date_validate = min(item for item in date_list if item > today)
+        return date_validate
 
     def older_product(dict_list):
         date_list = []
@@ -15,12 +32,36 @@ class SimpleReport:
         return date_list
 
     def high_stock(dict_list):
-        return dict_list
+        enterprises_stock_value = []
+
+        for enterprise in dict_list:
+            stock_value = 0
+            stock_value = SimpleReport.count_enterprise(
+                enterprise["nome_da_empresa"], dict_list
+                )
+            enterprises_stock_value.append(
+                {"nome_da_empresa": enterprise["nome_da_empresa"],
+                    "valor_objetos_estocados": stock_value})
+        # https://raccoon.ninja/pt/dev-pt/ordenar-lista-de-dicionarios-pelos-valores-das-chaves-python/
+        ordenados = sorted(
+            enterprises_stock_value, key=lambda k: k['valor_objetos_estocados']
+            )
+        return ordenados[len(ordenados) - 1]
 
     def generate(dict_list):
         old_product = min(SimpleReport.older_product(dict_list))
-        print(SimpleReport.next_date(dict_list), 'estou')
-        first_line = (
+        expiration_date = SimpleReport.closest_expiration_date(dict_list)
+        enterprise_name = SimpleReport.high_stock(dict_list)
+        one_line = (
             f"Data de fabricação mais antiga: {old_product}"
             )
-        return first_line
+        two_line = (
+            f"Data de validade mais próxima: {expiration_date}"
+        )
+        three_line = (
+            f"""Empresa com maior quantidade de produtos estocados: {
+                enterprise_name["nome_da_empresa"]
+                }"""
+            )
+
+        return f"{one_line}\n{two_line}\n{three_line}\n"
