ab3b9db73dc3f0a4cb0ec5df95f960ee89d8082a
EderPaiva <edergomesdepaiva12@gmail.com>
2022-01-03 21:35:39 -0300

    requisito 3 4 5 completo mas o xml me deu trabalho

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,3 +1,4 @@
+import xml.etree.ElementTree as Et
 import csv
 import json
 from pathlib import Path
@@ -15,10 +16,10 @@ class Inventory:
         return metodo_do_report.generate(arquivo_parsed)
 
     def report_method(report_type):
-        if report_type == "simples":
-            return SimpleReport
-        else:
+        if report_type == "completo":
             return CompleteReport
+        else:
+            return SimpleReport
 
     def read_file_method(extensao, path):
         if extensao == ".csv":
@@ -27,8 +28,8 @@ class Inventory:
         elif extensao == ".json":
             return Inventory.read_file_json(path)
 
-        # elif extensao == ".xml":
-        #     return Inventory.read_file_xml(path)
+        elif extensao == ".xml":
+            return Inventory.read_file_xml(path)
 
     def read_file_csv(path):
         with open(path) as file:
@@ -39,3 +40,24 @@ class Inventory:
         with open(path, 'r') as file:
             file_content = json.load(file)
         return file_content
+
+    def read_file_xml(path):
+        dom = Et.parse(path)
+        records = dom.findall("record")
+        # filtro = "*"
+        lista = []
+        # https://www.youtube.com/watch?v=rFxXDO8-keg (importante para aprender xml parser!)
+        for record in records:
+            lista.append({
+                "id": record.find("id").text,
+                "nome_do_produto": record.find("nome_do_produto").text,
+                "nome_da_empresa": record.find("nome_da_empresa").text,
+                "data_de_fabricacao": record.find("data_de_fabricacao").text,
+                "data_de_validade": record.find("data_de_validade").text,
+                "numero_de_serie": record.find("numero_de_serie").text,
+                "instrucoes_de_armazenamento": record.find(
+                    "instrucoes_de_armazenamento").text
+                })
+
+        print(lista)
+        return lista
